879e45a8470c771ebc8928483d478d54183dd51e
skitzoidman <lschmalzl@web.de>
2019-01-23 05:22:05 -0800

    windows10 ubuntu_laptop manual merge

---
+++ ex40a.py
@@ -0,0 +1,23 @@
+import mystuff
+mystuff.apple()
+MyStuff = {'apple': "I AM APPLES"}
+
+#mystuff = {'apple': "I AM APPLES!"}
+#print(mystuff['apple'])
+
+print(mystuff.tangerine)
+
+#mystuff['apple']
+mystuff.apple()
+mystuff.tangerine
+
+class MyStuff(object):
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+	
+	def apple(self):
+		print("I AM CLASSY APPLES!")
+
+thing = MyStuff()
+thing.apple()
+#print(thing.tangerine)
