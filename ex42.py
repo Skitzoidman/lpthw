e4d8db604693b6d50bc450ef2cb72fdb0fa19b17
skit <skit@skitPC.Speedport_W_723V_1_46_000>
2019-02-19 21:01:55 +0100

    Adding new ex42 and ex42_1.py

--- ex42.py
+++ ex42.py
@@ -1,12 +1,8 @@
 ## Animal is a object (yes, sort of confusing) look at the extra credit
 class Animal(object):
-    something = "lol"
-    def __init__(self, Animal_something):
-        self.something = Animal_something
-    def print_something(self):
-        print(self.something)
+#    color_dict = {"1":"black", "2":"red", "3":"white", "4":"grey", "5":"tri-color"}
     pass
-
+                
 ## Dog is-a/inherits from Animal and has-a __init__ that takes self and name params 
 class Dog(Animal):
     def __init__(self, name):
@@ -15,12 +11,17 @@ class Dog(Animal):
 
 ## Cat is-a/inherits from Animal and has-a __init__ that takes self and name params
 class Cat(Animal):
-    def __init__(self, name):
+#    color_list = ["black","red", "white", "grey", "tri-color"]
+    color_dict = {"1":"black", "2":"red", "3":"white", "4":"grey", "5":"tri-color"}
+    def __init__(self, name, color_choice):
         ## get name-attribute from self-attribute and set it to variable "name"
         self.name = name
+        self.color_choice = color_choice
+    def print_color(self):
+#        print(self.color_list[self.color_choice])
+        print(self.color_dict[self.color_choice])
     def print_name(self):
         print(self.name)
-
 
 ## Person is-a/inherits from object and has-a __init__ that takes self and name params
 class Person(object):
@@ -58,14 +59,32 @@ class Halibut(Fish):
 rover = Dog("Rover")
 
 ## satan is-a/instance Cat and name-attribute is set to "Satan"
-satan = Cat("Satan")
-satan.print_name()
-satan.print_something()
+#satan = Cat("Satan")
+#satan = Cat("Satan", -1)
+#satan.print_name()
+#satan.print_color()
+
+#name and color of cat can be choosen by the user
+print("Please enter the cats name:")
+cat_name = input("> ")
+print("Name is:", cat_name)
+spc_cat = str(cat_name)
+#print("Choose color from the following list:", Cat("test", 0).color_list)
+print("Choose color from the following list:", Cat("test", 0).color_dict)
+#input for list
+#cat_color = int(input("> "))
+#input for dict
+cat_color = input("> ")
+spc_cat = Cat(cat_name, cat_color)
+spc_cat.print_name()
+spc_cat.print_color()
+
 ## mary is-a/instance of Person and name-attribute is set to "Mary"
 mary = Person("Mary")
 
 ## get pet-attribute from instance mary and set it to "satan"
-mary.pet = satan
+#mary.pet = satan
+mary.pet = spc_cat
 
 ## frank is-a/instance of Employee, name-attribute is set to "Frank", salary-attribute is set to "120000"
 frank = Employee("Frank", 120000)
@@ -74,6 +93,7 @@ frank = Employee("Frank", 120000)
 frank.pet = rover
 frank.print_name()
 frank.print_pet()
+
 ## instance "flipper" is-a/inherits from Fish
 flipper = Fish()
 
---
+++ ex42_1.py
@@ -0,0 +1,38 @@
+class Animal(object):
+#   color_dict = {"1":"black", "2":"red", "3":"white", "4":"grey", "5":"tri-color"}
+#    def __init__(self, color_choice):
+    pass 
+    
+class Dog(Animal):
+    def __init__(self, name):
+       self.name = name
+
+class Cat(Animal):
+    color_dict = {"1":"black", "2":"red", "3":"white", "4":"grey", "5":"tri-color"}
+    def __init__(self, name, color_choice):
+        self.name = name
+#        self.color_dict = color_dict
+        self.color_choice = color_choice
+    def print_color(self):
+        print(self.color_dict[self.color_choice])
+    def print_name(self):
+        print(self.name)
+
+class Hybrid(Cat, Dog):
+    pass
+ 
+#name and color of cat can be choosen by the user
+print("Please enter the cats name:")
+cat_name = input("> ")
+print("Name is:", cat_name)
+spc_cat = str(cat_name)
+print("Choose color from the following list:", Cat("test", 0).color_dict)
+#input for dict
+cat_color = input("> ")
+spc_cat = Cat(cat_name, cat_color)
+spc_cat.print_name()
+spc_cat.print_color()
+
+hybrid = Hybrid("test", "3")
+hybrid.print_name()
+hybrid.print_color()
